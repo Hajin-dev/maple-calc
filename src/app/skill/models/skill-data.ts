d2aa729350dfe4c4783bcfea67c4f7a564c334f4
Hajin-dev <hajin1254@protonmail.com>
2022-06-12 13:15:43 +0900

    add soulmaster remastered Data

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -7399,7 +7399,214 @@ export const chtrSkillData: SkillData[] = [
       x: 435,
       y: 256,
     },
+  },{name: '솔라 슬래시',
+  location: 'new-assets/SMsolarSlash.png',
+  job: '소울마스터(리마스터)',
+  lt: {
+    x: -220,
+    y: -150,
   },
+  rb: {
+    x: 20,
+    y: 30,
+  },
+  origin: {
+    x: 252,
+    y: 171,
+  },
+},{name: '루나 디바이드',
+location: 'new-assets/SMlunarDivide.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -250,
+  y: -165,
+},
+rb: {
+  x: 20,
+  y: 35,
+},
+origin: {
+  x: 121,
+  y: 150,
+},
+},{name: '솔라 슬래시(2차)',
+location: 'new-assets/SMsolarSlash2.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -310,
+  y: -160,
+},
+rb: {
+  x: 20,
+  y: 40,
+},
+origin: {
+  x: 356,
+  y: 174,
+},
+},{name: '루나 디바이드(2차)',
+location: 'new-assets/SMlunarDivide2.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -310,
+  y: -185,
+},
+rb: {
+  x: 20,
+  y: 55,
+},
+origin: {
+  x: 146,
+  y: 168,
+},
+},{name: '소울 슬래시(3차)',
+location: 'new-assets/SMsolarSlash3.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -360,
+  y: -165,
+},
+rb: {
+  x: 20,
+  y: 55,
+},
+origin: {
+  x: 356,
+  y: 178,
+}},{name: '루나 디바이드(3차)',
+location: 'new-assets/SMlunarDivide3.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -360,
+  y: -185,
+},
+rb: {
+  x: 20,
+  y: 65,
+},
+origin: {
+  x: 177,
+  y: 203,
+},
+},{name: '소울 슬래시(4차)',
+location: 'new-assets/SMsolarSlash4.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -400,
+  y: -180,
+},
+rb: {
+  x: 20,
+  y: 80,
+},
+origin: {
+  x: 447,
+  y: 179,
+}},{name: '루나 디바이드(4차)',
+location: 'new-assets/SMlunarDivide4.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -400,
+  y: -205,
+},
+rb: {
+  x: 20,
+  y: 80,
+},
+origin: {
+  x: 221,
+  y: 235,
+},
+},{name: '코스믹 매터-범위?(2차)',
+location: 'new-assets/SMcosmicVoid.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -130,
+  y: -130,
+},
+rb: {
+  x: 130,
+  y: 130,
+},
+origin: {
+  x: 218,
+  y: 210,
+}},{name: '코스믹 샤워-설치(3차)',
+location: 'new-assets/SMcosmicShower.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -300,
+  y: -250,
+},
+rb: {
+  x: 300,
+  y: 80,
+},
+origin: {
+  x: 393,
+  y: 267,
+},
+},{name: '코스믹 버스트-추적(4차)',
+location: 'new-assets/SMcosmicBurst.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -450,
+  y: -450,
+},
+rb: {
+  x: 450,
+  y: 450,
+},
+origin: {
+  x: 393,
+  y: 433,
+},
+},{name: '블레이징 어썰트',
+location: 'new-assets/blazingAssault.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -500,
+  y: -180,
+},
+rb: {
+  x: 50,
+  y: 70,
+},
+origin: {
+  x: 514,
+  y: 226,
+},
+},{name: '러스터 차지',
+location: 'new-assets/blazingAssault.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -500,
+  y: -180,
+},
+rb: {
+  x: 50,
+  y: 70,
+},
+origin: {
+  x: 602,
+  y: 230,
+},
+},{name: '코스모스',
+location: 'new-assets/cosmos.png',
+job: '소울마스터(리마스터)',
+lt: {
+  x: -380,
+  y: -340,
+},
+rb: {
+  x: 380,
+  y: 80,
+},
+origin: {
+  x: 524,
+  y: 406,
+},
+}
 ];
 
 export const skillData = [...chtrSkillData, ...bossSkillData];
