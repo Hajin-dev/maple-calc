7629b9deee2c0bcb13e11b68493f12ba2abbea5e
Hajin-dev <hajin1254@protonmail.com>
2022-06-12 11:01:57 +0900

    add Mikhail

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -7193,6 +7193,196 @@ export const chtrSkillData: SkillData[] = [
       y: 242,
     },
     scale: 200,
+  }, 
+    {
+    name: '로얄가드-피격범위',
+    location: 'new-assets/new-royalguard.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -90,
+      y: -95,
+    },
+    rb: {
+      x: 40,
+      y: 10,
+    },
+    origin: {
+      x: 239,
+      y: 233,
+    },
+  },   {
+    name: '로얄가드 5단계',
+    location: 'new-assets/new-royalguard5-hit.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -370,
+      y: -380,
+    },
+    rb: {
+      x: 340,
+      y: 200,
+    },
+    origin: {
+      x: 402,
+      y: 519,
+    },
+  },
+     {
+    name: '샤이닝 피어스(1차)',
+    location: 'new-assets/shiningPierce.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -290,
+      y: -115
+    },
+    rb: {
+      x: 0,
+      y: 40,
+    },
+    origin: {
+      x: 406,
+      y: 138,
+    },
+  },
+  {name: '소울 어썰트(2차)',
+    location: 'new-assets/new-soulAssault.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -310,
+      y: -140,
+    },
+    rb: {
+      x: 20,
+      y: 40,
+    },
+    origin: {
+      x: 547,
+      y: 252,
+    },
+  },
+    {name: '소울 릴리즈(3차)',
+    location: 'new-assets/soulRelease.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -335,
+      y: -175,
+    },
+    rb: {
+      x: 5,
+      y: 55,
+    },
+    origin: {
+      x: 425,
+      y: 243,
+    },
+  },
+  {name: '소울 링크-파티원 적용범위',
+    location: 'new-assets/new-soulLink1.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -300,
+      y: -250,
+    },
+    rb: {
+      x: 300,
+      y: 250,
+    },
+    origin: {
+      x: 82,
+      y: 50,
+    },
+  },  {
+    name: '오펜시브 디펜스 추가타',
+    location: 'new-assets/offensiveDefense.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -335,
+      y: -140,
+    },
+    rb: {
+      x: 10,
+      y: 110,
+    },
+    origin: {
+      x: 337,
+      y: 173,
+    },
+  },  {
+    name: '샤이닝 크로스',
+    location: 'new-assets/new-shiningCross.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -400,
+      y: -200,
+    },
+    rb: {
+      x: 30,
+      y: 130,
+    },
+    origin: {
+      x: 642,
+      y: 270,
+    },
+  },{name: '샤이닝 크로스(어썰트)',
+    location: 'new-assets/shiningAssault.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -300,
+      y: -160,
+    },
+    rb: {
+      x: 20,
+      y: 50,
+    },
+    origin: {
+      x: 445,
+      y: 248,
+    },
+  },{name: '인스톨 쉴드(공격)',
+    location: 'new-assets/installShield.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -285,
+      y: -350,
+    },
+    rb: {
+      x: 285,
+      y: 50,
+    },
+    origin: {
+      x: 179,
+      y: 330,
+    },
+  },{name: '데들리 차지',
+    location: 'new-assets/new-deadlyCharge.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -500,
+      y: -400,
+    },
+    rb: {
+      x: 100,
+      y: 60,
+    },
+    origin: {
+      x: 796,
+      y: 543,
+    },
+  },{name: '소울 마제스티(빛의 파동)',
+    location: 'new-assets/soulMajesty.png',
+    job: '미하일(리마스터)',
+    lt: {
+      x: -425,
+      y: -185,
+    },
+    rb: {
+      x: 0,
+      y: 150,
+    },
+    origin: {
+      x: 435,
+      y: 256,
+    },
   },
 ];
 
