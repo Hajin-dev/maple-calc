80b3a586b3e032bce6fc3f3ffaea57e8431cd608
Hajin-dev <hajin1254@protonmail.com>
2022-06-12 14:20:48 +0900

    add remastered NightWalker data

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -7846,6 +7846,51 @@ origin: {
   x: 197,
   y: 369,
 },
+},{name: '럭키세븐(1차)',
+location: 'new-assets/NWluckySeven.png',
+job: '나이트워커(리마스터)',
+lt: {
+  x: -300,
+  y: -145,
+},
+rb: {
+  x: 10,
+  y: 5,
+},
+origin: {
+  x: 380,
+  y: 194,
+},
+},{name: '다크니스 오멘(설치)',
+location: 'new-assets/NWdarknessOmen.png',
+job: '나이트워커(리마스터)',
+lt: {
+  x: -300,
+  y: -300,
+},
+rb: {
+  x: 300,
+  y: 10,
+},
+origin: {
+  x: 502,
+  y: 469,
+},
+},{name: '쉐도우 스티치(적용 범위?)',
+location: 'new-assets/NWshadowStitch.png',
+job: '나이트워커(리마스터)',
+lt: {
+  x: -340,
+  y: -430,
+},
+rb: {
+  x: 340,
+  y: 170,
+},
+origin: {
+  x: 276,
+  y: 220,
+},
 }
 ];
 
