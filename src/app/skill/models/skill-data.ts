ec460227834ad0f2ec4077d3a609a6d9b507009d
Hajin-dev <hajin1254@protonmail.com>
2022-06-12 13:38:22 +0900

    add reamastered flameWizard data

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -7606,6 +7606,111 @@ origin: {
   x: 524,
   y: 406,
 },
+},{name: '플레임 바이트(1차)',
+location: 'new-assets/FWflameWind.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -310,
+  y: -90,
+},
+rb: {
+  x: 0,
+  y: 40,
+},
+origin: {
+  x: 401,
+  y: 139,
+},
+},{name: '플레임 볼텍스(2차)',
+location: 'new-assets/FWfireRelease.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -420,
+  y: -170,
+},
+rb: {
+  x: 30,
+  y: 20,
+},
+origin: {
+  x: 573,
+  y: 275,
+},
+},{name: '플레임 템페스타(3차)',
+location: 'new-assets/FWblazingStrike.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -360,
+  y: -167,
+},
+rb: {
+  x: 355,
+  y: 62,
+},
+origin: {
+  x: 478,
+  y: 219,
+},
+},{name: '마엘스트롬(범위)',
+location: 'new-assets/FWcolossusBomb.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -300,
+  y: -340,
+},
+rb: {
+  x: 300,
+  y: 60,
+},
+origin: {
+  x: 407,
+  y: 325,
+},
+},{name: '인페르노라이즈(라이온)',
+location: 'new-assets/FWignition.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -440,
+  y: -430,
+},
+rb: {
+  x: 440,
+  y: 200,
+},
+origin: {
+  x: 621,
+  y: 220,
+},
+},{name: '인페르노라이즈(폭스)',
+location: 'new-assets/FWinfernoFox.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -170,
+  y: -100,
+},
+rb: {
+  x: 80,
+  y: 40,
+},
+origin: {
+  x: 214,
+  y: 392,
+},
+},{name: '버닝리전',
+location: 'new-assets/FWburningRegion.png',
+job: '플레임위자드(리마스터)',
+lt: {
+  x: -300,
+  y: -150,
+},
+rb: {
+  x: 300,
+  y: 150,
+},
+origin: {
+  x: 461,
+  y: 204,
+},
 }
 ];
 
