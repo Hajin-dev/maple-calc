6dc613b1ea189790c903a6beb0e4a61724ef44a9
icepeng <typ0@naver.com>
2022-06-12 02:53:32 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Hajin-dev/maple-calc/commit/dfb6b88d3048cc334a44a06fb90c29e35e6a672c

--- src/app/deal/models/core.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Core {
-  type: 'UPGRADE' | 'SKILL' | 'SPECIAL';
-  level: number;
-  slotLevel: number;
-  skillCode?: string;
-}
--- src/app/skill/models/canvas-setting.ts
+++
@@ -1,4 +0,0 @@
-export interface CanvasSetting {
-  background: string;
-  showGrid: boolean;
-}
