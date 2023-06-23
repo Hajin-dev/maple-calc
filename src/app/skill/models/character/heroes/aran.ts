b24a474afb04b171627625da2898fe3cbd81d59a
Hajin-dev <hajin1254@protonmail.com>
2023-06-23 17:32:26 +0900

    aran update

--- src/app/skill/models/character/heroes/aran.ts
+++ src/app/skill/models/character/heroes/aran.ts
@@ -134,5 +134,157 @@ export const aranSkillData:SkillData[]=[
       x: 896,
       y: 584,
     },
+  }, {
+    name: '비욘더 VI(1타)',
+    location: 'assets/heroes/aran/6thBeyonder1.png',
+    job: '아란',
+    lt: {
+      x: -450,
+      y: -270,
+    },
+    rb: {
+      x: 25,
+      y: 80,
+    },
+    origin: {
+      x: 566,
+      y: 497,
+    },
   },
+  {
+    name: '비욘더 VI(2타)',
+    location: 'assets/heroes/aran/6thBeyonder2.png',
+    job: '아란',
+    lt: {
+      x: -450,
+      y: -270,
+    },
+    rb: {
+      x: 25,
+      y: 80,
+    },
+    origin: {
+      x: 554,
+      y: 368,
+    },
+  },
+  {
+    name: '비욘더 VI(3타)',
+    location: 'assets/beyonder3.png',
+    job: '아란',
+    lt: {
+      x: -450,
+      y: -270,
+    },
+    rb: {
+      x: 25,
+      y: 80,
+    },
+    origin: {
+      x: 529,
+      y: 423,
+    },
+  },
+  {
+    name: '비욘더 VI - 마하 적용 범위',
+    location: 'assets/beyonder3.png',
+    job: '아란',
+    lt: {
+      x: -500,
+      y: -400,
+    },
+    rb: {
+      x: 500,
+      y: 400,
+    },
+    origin: {
+      x: 120,
+      y: 247,
+    },
+  },
+  {
+    name: '아드레날린 서지 - 하울링 스윙 (1타)',
+    location: 'assets/heroes/aran/6thAdrenalinSurgeSwing1.png',
+    job: '아란',
+    lt: {
+      x: -375,
+      y: -250,
+    },
+    rb: {
+      x: 35,
+      y: 105,
+    },
+    origin: {
+      x: 539,
+      y: 358,
+    },
+  },
+  {
+    name: '아드레날린 서지 - 하울링 스윙 (2타)',
+    location: 'assets/heroes/aran/6thAdrenalinSurgeSwing2.png',
+    job: '아란',
+    lt: {
+      x: -305,
+      y: -140,
+    },
+    rb: {
+      x: 265,
+      y: 70,
+    },
+    origin: {
+      x: 326,
+      y: 207,
+    },
+  },
+  {
+    name: '아드레날린 서지 - 하울링 스윙 (3타)',
+    location: 'assets/heroes/aran/6thAdrenalinSurgeSwing3.png',
+    job: '아란',
+    lt: {
+      x: -450,
+      y: -280,
+    },
+    rb: {
+      x: 35,
+      y: 135,
+    },
+    origin: {
+      x: 619,
+      y: 378,
+    },
+  },
+  {
+    name: '아드레날린 서지 - 하울링 스윙 (4타)',
+    location: 'assets/heroes/aran/6thAdrenalinSurgeSwing4.png',
+    job: '아란',
+    lt: {
+      x: -505,
+      y: -300,
+    },
+    rb: {
+      x: 90,
+      y: 135,
+    },
+    origin: {
+      x: 645,
+      y: 541,
+    },
+  },
+  {
+    name: '아드레날린 서지(파동)',
+    location: 'assets/heroes/aran/6thAdrenalinSurgeSwingWave.png',
+    job: '아란',
+    lt: {
+      x: -585,
+      y: -395,
+    },
+    rb: {
+      x: 65,
+      y: 135,
+    },
+    origin: {
+      x: 693,
+      y: 372,
+    },
+  }
 ]
