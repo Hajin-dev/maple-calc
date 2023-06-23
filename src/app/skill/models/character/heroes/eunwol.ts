c059b18d2381545bd374e34bb9678f5eb356b947
Hajin-dev <hajin1254@protonmail.com>
2023-06-23 18:06:07 +0900

    eunwol update

--- src/app/skill/models/character/heroes/eunwol.ts
+++ src/app/skill/models/character/heroes/eunwol.ts
@@ -187,4 +187,97 @@ export const eunwolSkillData:SkillData[]=[
       y: 523,
     },
   },
+  {
+    name: '귀참 VI',
+    location: 'assets/heroes/eunwol/6thSpiritClaw1.png',
+    job: '은월',
+    lt: {
+      x: -420,
+      y: -230,
+    },
+    rb: {
+      x: 100,
+      y: 60,
+    },
+    origin: {
+      x: 514,
+      y: 358,
+    },
+  },
+  {
+    name: '호신강림 - 호신연참(1타)',
+    location: 'assets/heroes/eunwol/6thAdventClaw1.png',
+    job: '은월',
+    lt: {
+      x: -520,
+      y: -290,
+    },
+    rb: {
+      x: 110,
+      y: 60,
+    },
+    origin: {
+      x: 845,
+      y: 482,
+    },
+  },
+  {
+    name: '호신강림 - 호신연참(2타)',
+    location: 'assets/heroes/eunwol/6thAdventClaw2.png',
+    job: '은월',
+    lt: {
+      x: -520,
+      y: -290,
+    },
+    rb: {
+      x: 110,
+      y: 60,
+    },
+    origin: {
+      x: 711,
+      y: 522,
+    },
+  },
+  {
+    name: '호신강림 - 호신연참(3타)',
+    location: 'assets/heroes/eunwol/6thAdventClaw3.png',
+    job: '은월',
+    lt: {
+      x: -520,
+      y: -290,
+    },
+    rb: {
+      x: 110,
+      y: 60,
+    },
+    origin: {
+      x: 958,
+      y: 497,
+    },
+  },
+  {
+    name: '호신강림 - 환여우령',
+    location: 'assets/heroes/eunwol/6thAdventClawFox.png',
+    job: '은월',
+    lt: {
+      x: -570,
+      y: -400,
+    },
+    rb: {
+      x: 150,
+      y: 80,
+    },
+    lt2: {
+      x: -450,
+      y: -300,
+    },
+    rb2: {
+      x: 0,
+      y: 5,
+    },
+    origin: {
+      x: 958,
+      y: 497,
+    },
+  },
 ]
